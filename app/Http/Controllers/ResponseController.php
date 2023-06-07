55e5a7d488551ef9140cc67f5e2bd48c776ac16a
Benyamin Rmb <benyaminrmb@gmail.com>
2023-06-07 23:22:40 +0330

    (feat):added new response controller.

---
+++ app/Http/Controllers/ResponseController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+
+class ResponseController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | generateResponse => Generate default Json response for client.
+    |--------------------------------------------------------------------------
+    |
+    | By this function you can handle your errors or success responses.
+    |
+    */
+    /**
+     * @param null $result
+     * @param bool $success
+     * @param null $message
+     * @param int $status_code
+     * @return JsonResponse
+     */
+    public function generateResponse(
+        $result = null,
+        bool $success = true,
+        $message = null,
+        int $status_code = 200
+    ): JsonResponse
+    {
+        $response = [
+            'success' => $success,
+        ];
+
+        if ($result !== null) {
+            $response['data'] = $result;
+        }
+        if ($message) {
+            $response['message'] = $message;
+        }
+
+        return response()->json($response, $status_code);
+    }
+}
