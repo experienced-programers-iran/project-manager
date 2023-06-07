6aea8fec13dd055bababc463f5d52857a2a47996
Benyamin Rmb <benyaminrmb@gmail.com>
2023-06-07 23:23:24 +0330

    (feat):create register method based on modular system, unfinished yet.

--- Modules/Auth/Http/Controllers/AuthController.php
+++ Modules/Auth/Http/Controllers/AuthController.php
@@ -2,78 +2,22 @@
 
 namespace Modules\Auth\Http\Controllers;
 
-use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Http\Request;
-use Illuminate\Routing\Controller;
+use App\Http\Controllers\ResponseController;
+use App\Models\User;
+use Modules\Auth\Http\Requests\RegisterRequest;
 
-class AuthController extends Controller
+class AuthController extends ResponseController
 {
-    /**
-     * Display a listing of the resource.
-     * @return Renderable
-     */
-    public function index()
+    public function register(RegisterRequest $request)
     {
-        return view('auth::index');
-    }
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+        ]);
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Renderable
-     */
-    public function create()
-    {
-        return view('auth::create');
-    }
+        $token = $user->createToken('AuthToken')->accessToken;
 
-    /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function show($id)
-    {
-        return view('auth::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function edit($id)
-    {
-        return view('auth::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Renderable
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
-     */
-    public function destroy($id)
-    {
-        //
+        return $this->generateResponse(result:$token );
     }
 }
---
+++ Modules/Auth/Http/Requests/RegisterRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Modules\Auth\Http\Requests;
+
+use App\Http\Controllers\ResponseController;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $response = (new ResponseController())->generateResponse(
+            result: $validator->errors(),
+            success: false,
+            status_code: 400
+        );
+
+
+        throw new HttpResponseException($response);
+    }
+}
