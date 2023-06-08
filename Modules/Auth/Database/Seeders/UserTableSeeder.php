8dde0f4e6d1fdc95c38442306119d41a0e6f1733
Benyamin Rmb <benyaminrmb@gmail.com>
2023-06-08 11:11:59 +0330

    (fix):move all users databases to Auth Module.

---
+++ Modules/Auth/Database/Seeders/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Auth\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Modules\Auth\Entities\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        User::factory(5)->create();
+        // $this->call("OthersTableSeeder");
+    }
+}
